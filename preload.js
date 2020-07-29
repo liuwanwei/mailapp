ffc5c130f8981ce90630c865c3d541dbe6fe25f2
liuwanwei <liuwanwei@gmail.com>
2020-07-29 15:24:22 +0800

    增加侦听 SIGHUP 信号，和显示 PID 到屏幕功能

--- preload.js
+++ preload.js
@@ -1,12 +1,30 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
+const replaceText = (selector, text) => {
+  const element = document.getElementById(selector)
+  if (element) {
+    element.innerText = text
+  }else{
+    console.log(`couldn't find element with id ${selector}`)
   }
+}
 
+window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+  
+  if (process.pid){
+    // 在调试 console 中打印
+    console.log('pid: ' + process.pid)
+    replaceText('pid', process.pid)
+  }
 })
+
+
+// 通过信号方式简单通信测试代码
+var counter = 1
+process.on('SIGHUP', () => {
+  replaceText('mail-code', counter.toString())
+  counter++
+});
